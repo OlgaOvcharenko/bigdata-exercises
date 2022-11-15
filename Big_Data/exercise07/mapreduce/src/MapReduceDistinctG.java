1b5b7245a5d24cea9708617aab395a553cef0d8c
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2022-11-15 01:01:52 +0100

    Add necessary files

---
+++ Big_Data/exercise07/mapreduce/src/MapReduceDistinctG.java
@@ -0,0 +1,96 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class MapReduceDistinctG {
+
+    private static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            // write your map function
+            String val = value.toString();
+            String[] words = val.split(" ");
+
+            // Set<> l = new ArrayList<>
+            for(String w : words) {
+                // Task 1
+                // if(w.startsWith("H")) {
+                //     word.set(w);
+                //     context.write(word, one);
+                // }
+
+                // Task 2
+                if(w.endsWith("g")) {
+                    word.set(w);
+                    context.write(word, one);
+                }
+            }
+        }
+    }
+
+    private static class WordCountReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            // write your reduce function
+            // int i = 0;
+            // for(IntWritable value : values) {
+            //     i += 1;
+            // }
+
+            // Task 1
+            // result.set(1);
+            // context.write(key, result);
+
+
+            // Task 2
+            int i = 0;
+            for(IntWritable value : values) {
+                i += value.get();
+            }
+            result.set(i);
+            context.write(key, result);
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: 2 required params <hdfs_file_in> <hdfs_file_out>");
+            System.exit(2);
+        }
+        // Setup a MapReduce job
+        Job job = new Job(conf, "word,count");
+        job.setJarByClass(MapReduceDistinctG.class);
+        job.setMapperClass(WordCountMapper.class);
+        
+	// Can you reducer be used as a combiner? If so, enable it.
+	// You can also experiment with and without combiner and compare the running time.
+	// job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+	// at least one reducer, experiment with this parameter
+        //job.setNumReduceTasks(8);    
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
